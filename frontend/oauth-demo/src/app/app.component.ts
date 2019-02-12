ebd1ef8826eae32ec3c80b23e15c9815255475b0
David Rehle <d.rehle@gmail.com>
2019-02-12 06:07:44 +0100

    base for login

--- frontend/oauth-demo/src/app/app.component.ts
+++ frontend/oauth-demo/src/app/app.component.ts
@@ -1,16 +1,34 @@
 import {Component} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 
+import {Router} from '@angular/router';
+import {AppService} from './app.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'oauth-demo';
-  greeting = {};
+  constructor(private app: AppService, private http: HttpClient, private router: Router) {
+    this.app.authenticate(undefined, undefined);
+  }
 
-  constructor(private http: HttpClient) {
-    http.get('resource').subscribe(data => this.greeting = data);
+  logout() {
+    this.http.post('logout',
+      {})
+      .subscribe(
+        (val) => {
+          console.log("POST call successful value returned in body",
+            val);
+          this.app.authenticated = false;
+          this.router.navigateByUrl('/login');
+        },
+        response => {
+          console.log("POST call in error", response);
+        },
+        () => {
+          console.log("The POST observable is now completed.");
+        });
   }
 }
