217b49ccc525a90f7376452560c4274a6affd89f
David Rehle <d.rehle@gmail.com>
2019-03-07 03:07:01 +0100

    authrequest to oauth endpoint

--- frontend-angular/src/app/login/login.component.ts
+++ frontend-angular/src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {AppService} from '../app.service';
 import {HttpClient} from '@angular/common/http';
 import {Router} from '@angular/router';
+import { AuthenticationService } from '../service/authentication.service';
 
 @Component({
   templateUrl: './login.component.html'
@@ -10,13 +11,17 @@ export class LoginComponent {
 
   credentials = {username: '', password: ''};
 
-  constructor(private app: AppService, private http: HttpClient, private router: Router) {
+  constructor(private app: AppService,
+              private authService: AuthenticationService,
+              private http: HttpClient,
+              private router: Router) {
   }
 
   login() {
-    this.app.authenticate(this.credentials, () => {
-      this.router.navigateByUrl('/').then();
-    });
-    return false;
+    this.authService.login(this.credentials.username, this.credentials.password);
+    // this.app.authenticate(this.credentials, () => {
+    //   this.router.navigateByUrl('/').then();
+    // });
+    // return false;
   }
 }
---
+++ frontend-angular/src/app/service/authentication.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  static AUTH_URL = 'http://localhost:8080/oauth/token';
+  static CLIENT_ID = 'testjwtclientid';
+  static CLIENT_SECRET = 'XY7kmzoNzl100';
+
+  constructor(private http: HttpClient) {
+  }
+
+
+  login(username: string, password: string) {
+    const body = `username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}&grant_type=password`;
+    const httpHeaders = new HttpHeaders()
+      .set('Content-Type', 'application/x-www-form-urlencoded')
+      .set('Authorization', 'Basic ' + btoa(AuthenticationService.CLIENT_ID + ':' + AuthenticationService.CLIENT_SECRET));
+
+    console.log('Authorization', 'Basic ' + btoa(AuthenticationService.CLIENT_ID + ':' + AuthenticationService.CLIENT_SECRET));
+
+    const options = {headers: httpHeaders};
+
+    return this.http.post(AuthenticationService.AUTH_URL, body, options).subscribe(res => {
+        console.log(res);
+      },
+      error1 => {
+        console.log('--------------->');
+        console.error(JSON.stringify(error1));
+      });
+  }
+}
