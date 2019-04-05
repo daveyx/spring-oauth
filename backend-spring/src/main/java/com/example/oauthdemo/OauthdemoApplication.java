f0d70dd1df928d00c6fdd5b9e700cd99b4568927
David Rehle <d.rehle@gmail.com>
2019-04-05 09:33:49 +0200

    support for logout

--- backend-spring/src/main/java/com/example/oauthdemo/OauthdemoApplication.java
+++ backend-spring/src/main/java/com/example/oauthdemo/OauthdemoApplication.java
@@ -1,12 +1,14 @@
 package com.example.oauthdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.web.bind.annotation.*;
 
-import java.security.Principal;
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -35,6 +37,20 @@ public class OauthdemoApplication {
         final Map<String, Object> model = new HashMap<>();
         model.put("content", "Hello unsecured World");
         return model;
+    }
+
+    @Autowired
+    private TokenStore tokenStore;
+
+    @RequestMapping(value = "/oauth/logout", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public void logout(final HttpServletRequest request) {
+        final String authHeader = request.getHeader("Authorization");
+        if (authHeader != null) {
+            final String tokenValue = authHeader.replace("Bearer", "").trim();
+            final OAuth2AccessToken accessToken = tokenStore.readAccessToken(tokenValue);
+            tokenStore.removeAccessToken(accessToken);
+        }
     }
 }
 
