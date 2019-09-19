21b37757f0c827d17c8baa5ab675f26e3bafd638
David Rehle <d.rehle@gmail.com>
2019-09-19 16:12:58 +0200

    use @SpringBootConfiguration instead of @SpringBootApplication

--- backend-spring/src/main/java/com/example/oauthdemo/OauthdemoApplication.java
+++ backend-spring/src/main/java/com/example/oauthdemo/OauthdemoApplication.java
@@ -1,16 +1,21 @@
 package com.example.oauthdemo;
 
 import com.example.oauthdemo.security.config1.SecurityConfig1Filter;
-import com.example.oauthdemo.security.config2.*;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigurationExcludeFilter;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.TypeExcludeFilter;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 
-@SuppressWarnings("SpringBootApplicationSetup")
-@SpringBootApplication
+
+@SpringBootConfiguration
+@EnableAutoConfiguration
 @ComponentScan(
         excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.CUSTOM, classes = TypeExcludeFilter.class),
+                @ComponentScan.Filter(type = FilterType.CUSTOM, classes = AutoConfigurationExcludeFilter.class),
                 @ComponentScan.Filter(type = FilterType.CUSTOM, classes = SecurityConfig1Filter.class)
         })
 public class OauthdemoApplication {
--- backend-spring/src/main/java/com/example/oauthdemo/OauthdemoApplication2.java
+++ backend-spring/src/main/java/com/example/oauthdemo/OauthdemoApplication2.java
@@ -2,13 +2,22 @@ package com.example.oauthdemo;
 
 import com.example.oauthdemo.security.config2.SecurityConfig2Filter;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigurationExcludeFilter;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.TypeExcludeFilter;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 
-@SuppressWarnings("SpringBootApplicationSetup")
-@SpringBootApplication
-@ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.CUSTOM, classes = SecurityConfig2Filter.class))
+
+@SpringBootConfiguration
+@EnableAutoConfiguration
+@ComponentScan(
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.CUSTOM, classes = TypeExcludeFilter.class),
+                @ComponentScan.Filter(type = FilterType.CUSTOM, classes = AutoConfigurationExcludeFilter.class),
+                @ComponentScan.Filter(type = FilterType.CUSTOM, classes = SecurityConfig2Filter.class)
+        })
 public class OauthdemoApplication2 {
     public static void main(String[] args) {
         SpringApplication.run(OauthdemoApplication2.class, args);
