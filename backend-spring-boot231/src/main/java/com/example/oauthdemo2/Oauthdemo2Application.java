9527d2d30db57e2a146749308e01e64d5c3d40f2
David Rehle <d.rehle@gmail.com>
2020-07-17 22:59:05 +0200

    start impl. with spring-authorization-server

---
+++ backend-spring-boot231/src/main/java/com/example/oauthdemo2/Oauthdemo2Application.java
@@ -0,0 +1,13 @@
+package com.example.oauthdemo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauthdemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauthdemo2Application.class, args);
+	}
+
+}
---
+++ backend-spring-boot231/src/test/java/com/example/oauthdemo2/Oauthdemo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oauthdemo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauthdemo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
