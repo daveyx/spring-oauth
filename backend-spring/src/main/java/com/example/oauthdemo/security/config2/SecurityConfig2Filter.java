fa3b47aae7d296803e9950f7763665bf54aca74e
David Rehle <d.rehle@gmail.com>
2020-07-28 09:30:48 +0200

    fixed filtering

--- backend-spring/src/main/java/com/example/oauthdemo/security/config2/SecurityConfig2Filter.java
+++ backend-spring/src/main/java/com/example/oauthdemo/security/config2/SecurityConfig2Filter.java
@@ -1,23 +1,24 @@
 package com.example.oauthdemo.security.config2;
 
+import com.example.oauthdemo.OAuthDemoApplication;
 import org.springframework.core.type.ClassMetadata;
 import org.springframework.core.type.classreading.MetadataReader;
 import org.springframework.core.type.classreading.MetadataReaderFactory;
 import org.springframework.core.type.filter.TypeFilter;
 
-import java.io.IOException;
 
 public class SecurityConfig2Filter implements TypeFilter {
 
     @Override
-    public boolean match(MetadataReader metadataReader, MetadataReaderFactory metadataReaderFactory) throws IOException {
+    public boolean match(MetadataReader metadataReader, MetadataReaderFactory metadataReaderFactory) {
         ClassMetadata classMetadata = metadataReader.getClassMetadata();
         String className = classMetadata.getClassName();
         if (className.startsWith("com.example.oauthdemo.security.config1.") ||
-                className.equals("com.example.oauthdemo.OauthdemoApplication")) {
+                className.equals(OAuthDemoApplication.class.getName())) {
             return true;
         }
 
         return false;
     }
+
 }
