e2947da82a9209df27b240d6cf04808a12aac471
David <d.rehle@gmail.com>
2021-06-05 05:03:54 +0200

    cleanup

--- backend-spring/src/main/java/com/example/oauthdemo/security/ResourceServerConfig.java
+++ backend-spring/src/main/java/com/example/oauthdemo/security/ResourceServerConfig.java
@@ -1,6 +1,5 @@
 package com.example.oauthdemo.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,13 +13,15 @@ import org.springframework.security.oauth2.provider.token.ResourceServerTokenSer
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
-    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
-    @Autowired
-    private ResourceServerTokenServices tokenServices;
-
     @Value("${security.jwt.resource-ids}")
     private String resourceIds;
 
+    private final ResourceServerTokenServices tokenServices;
+
+
+    public ResourceServerConfig(ResourceServerTokenServices tokenServices) {
+        this.tokenServices = tokenServices;
+    }
 
     @Override
     public void configure(final ResourceServerSecurityConfigurer resources) {
