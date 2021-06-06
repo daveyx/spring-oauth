b6d5d578dc36991b6aa358feff58589b8f7ff165
David <d.rehle@gmail.com>
2021-06-06 13:22:33 +0200

    externalized security.properties

--- backend-spring/src/main/java/com/example/oauthdemo/OAuthDemoApplication.java
+++ backend-spring/src/main/java/com/example/oauthdemo/OAuthDemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.TypeExcludeFilter;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 
 @SpringBootConfiguration
@@ -16,6 +18,9 @@ import org.springframework.context.annotation.FilterType;
                 @ComponentScan.Filter(type = FilterType.CUSTOM, classes = TypeExcludeFilter.class),
                 @ComponentScan.Filter(type = FilterType.CUSTOM, classes = AutoConfigurationExcludeFilter.class)
         })
+@PropertySources({
+        @PropertySource("classpath:security.properties")
+})
 public class OAuthDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(OAuthDemoApplication.class, args);
